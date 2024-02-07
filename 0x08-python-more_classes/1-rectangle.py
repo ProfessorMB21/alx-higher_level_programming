a48dba8caf4ed5ba805f1a3658ba845ae748c9b7
Bupe M <profbupeeli@gmail.com>
2024-02-07 12:00:32 +0300

    fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -82,6 +82,6 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height == value
+        self.__height = value
 
 
