6f30732368a68d5db5cf3275f4744ecbbccb2a9d
Bupe M <profbupeeli@gmail.com>
2024-02-23 03:02:57 +0300

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Definition of class Square inheriting from class Rectangle
+"""
+
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A Square class inheriting from base class Rectangle
+    """
+
+    def __init__(self, size):
+        """
+        Instatiation with size
+        Validates size
+
+        Args:
+            size (int): size of square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the area of the square
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Returns a string representation of the shape
+        """
+        return str("[Square] {}/{}".format(self.__size, self.__size))
+
